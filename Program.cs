06dbc457f831718e9acbfde8f431a416feab980b
exiw <51192517+exiw@users.noreply.github.com>
2021-09-21 20:08:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CumledenHarfSilme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder metin = new StringBuilder(" Kodlab yayınevi bünyesinde birçok kitap bulunmaktadır.");
+            metin = metin.Remove(0, 10);
+            Console.Write(metin);
+            Console.ReadKey();
+        }
+    }
+}
